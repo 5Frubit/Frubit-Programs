c7698193ec2083ad1041fb73820509ef562d4321
5Frubit <5frubit@gmail.com>
2024-03-08 23:38:00 +0100

    casino_cardwrite

--- casino/casino_server.lua
+++ casino/casino_server.lua
@@ -1,1 +1,18 @@
-print("Server Start!")
+local io = require("io")
+local key = require("component").keyboard
+local wr = require("component").os_cardwriter
+local run = true
+print("Server Started!")
+print("Hold C to enter the new card mode")
+while run do
+	if key.isKeyDown("c") then
+		cardSetup()
+	end
+end
+
+function cardSetup()
+	print("New card setup")
+	print("Insert card and enter name:")
+	name = io.read()
+	wr.write(name,0..","..name)
+end
