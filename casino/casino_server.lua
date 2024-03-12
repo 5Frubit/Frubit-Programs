0902dfa3c8600b41bc16fc6162706cea1fb2d06e
5Frubit <5frubit@gmail.com>
2024-03-12 21:09:32 +0100

    Casino Set/Get/Remove cred

--- casino/casino_server.lua
+++ casino/casino_server.lua
@@ -4,18 +4,64 @@ local wr = require("component").os_cardwriter
 local os = require("os")
 local run = true
 print("Server Started!")
-print("Hold C to enter the new card mode")
+print("Hold Q to enter the new card mode")
+print("Hold W to enter the getCred mode")
+print("Hold E to enter the setCred mode")
+print("Hold D to enter the removeCred mode")
+
+
+--playerCredDB Sync
+playerCredDB = {};
+
+
 
 function cardSetup()
 	print("New card setup")
 	print("Insert card and enter name:")
 	name = io.read()
-	wr.write(name.."/".."0",name)
+	wr.write(name,name)
+	playerCredDB[name] = 0
+end
+
+function updateCred()
+	
+end
+
+function setCred(name,amount)
+	playerCredDB[name] = amount
+end
+
+function getCred(name)
+	return playerCredDB[name]
+end
+
+function removeCred(name,amount)
+	playerCredDB[name] = playerCredDB[name] - amount;
 end
 
 while run do
-	if key.isKeyDown("c") then
+	if key.isKeyDown("q") then
 		cardSetup()
 	end
+	if key.isKeyDown("w")
+		print("getCred?:")
+		name = io.read()
+		print(getCred(name))
+	end
+	if key.isKeyDown("e")
+		print("setCred?:")
+		name = io.read()
+		print("amount?:")
+		amount = io.read()
+		setCred(name,amount)
+	end
+	if key.isKeyDown("d")
+		print("removeCred?:")
+		name = io.read()
+		print("amount?:")
+		amount = io.read()
+		removeCred(name,amount)
+	end
+	updateCred()
 	os.sleep(0.1)
 end
